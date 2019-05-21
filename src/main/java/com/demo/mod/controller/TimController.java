c597dca0f9b8ffa91ff51221b3788ce3b1e63dc9
唐鸣泽丨 <oncwnuck7itnt5auadkvhns9pxrk@git.weixin.qq.com>
2019-05-21 13:51:19 +0800

    3

--- src/main/java/com/demo/mod/controller/TimController.java
+++ src/main/java/com/demo/mod/controller/TimController.java
@@ -15,4 +15,20 @@ public class TimController {
     public String signIn(){
         return "login";
     }
+    @RequestMapping("/zw")
+    public String zw(){
+        return "zw";
+    }
+    @RequestMapping("/person")
+    public String person(){
+        return "person";
+    }
+    @RequestMapping("/dep")
+    public String dep(){
+        return "dep";
+    }
+    @RequestMapping("/zzdj")
+    public String zzdj(){
+        return "zzdj";
+    }
 }
